2f94732dd9e7c5d78aefaebe2020d94076e0f213
Roman <slyusarrv@yandex.ru>
2022-03-17 21:23:55 +0500

    some edits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import './App.css';
-import AuthorList from "./components/author_list";
 import axios from 'axios'
 import Menu from "./components/menu";
 import Footer from "./components/footer";
@@ -11,7 +10,6 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      "authors": [],
       "users": [],
     }
   }
@@ -20,21 +18,9 @@ class App extends React.Component {
   // Тогда и идем за данными
   componentDidMount() {
     axios
-      .get('http://127.0.0.1:8000/api/authors/')
+      .get('http://127.0.0.1:8000/api/users/')
       .then(responce => {
-        const authors = responce.data
-
-        // Поменяли состояние у реакта
-        this.setState({
-          'authors': authors
-        })
-      })
-      .catch(error => console.log(error))
-
-    axios
-      .get('http://127.0.0.1:8000/api/todousers/')
-      .then(responce => {
-        const users = responce.data
+        const users = responce.data.results
         // Поменяли состояние у реакта
         this.setState({
           'users': users
@@ -48,7 +34,6 @@ class App extends React.Component {
       <div>
         <Footer/>
         <Menu/>
-        <AuthorList authors={this.state.authors}/>
         <UserList users={this.state.users}/>
       </div>
     )
