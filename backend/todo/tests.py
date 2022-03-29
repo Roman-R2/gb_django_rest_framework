1bb3ba9ede7d3a73fa8dc8f20b53e33f88950ca5
Roman <slyusarrv@yandex.ru>
2022-03-29 22:57:06 +0500

    new tests

--- backend/todo/tests.py
+++ backend/todo/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+# генерит requests для нашего api
+from rest_framework.test import APIRequestFactory
+from rest_framework import status
 
-# Create your tests here.
+from todo.views import ProjectsViewSet
+
+
+class TestUserApi(TestCase):
+
+    def test_get_list(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/users')
+        view = ProjectsViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 0)
